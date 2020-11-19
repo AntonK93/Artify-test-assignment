409b8a962a9843ca121c509837a266d39e3573fc
AntonK93 <antonkukinn@gmail.com>
2020-11-19 15:54:28 +0200

    remove console

--- vue/src/store/store.js
+++ vue/src/store/store.js
@@ -22,9 +22,6 @@ const store = new Vuex.Store({
     state: {
         loading: false,
     },
-    created() {
-        console.log('test');
-    },
     mutations: {
         loading(state, loading) {
             state.loading = loading;
